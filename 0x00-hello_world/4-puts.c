e5751517e8406bee6b482dcb8fbf8ce8bff49043
KayWide <elkiariey@gmail.com>
2022-09-09 10:58:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/* int main is the entry point
- * puts prints the argument
- * retun (0) returns a value of 0*/
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
