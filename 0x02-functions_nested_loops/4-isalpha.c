1c9c3a47052b2b09c4972b6f7d87674b47112a94
KayWide <elkiariey@gmail.com>
2022-09-14 15:05:23 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 #include <ctype.h>
 #include "main.h"
 
@@ -12,11 +12,11 @@ int _isalpha(int c);
 {
 	if isalpha(c);
 	{
-	return(1)
+	return(1);
 	}
 	else
 	{
-	return(0)
+	return(0);
 	}
 }
 
