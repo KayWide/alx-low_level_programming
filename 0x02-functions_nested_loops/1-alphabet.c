e98d26b1aed2b9c0c288d863fa8e574884b26479
KayWide <elkiariey@gmail.com>
2022-09-14 13:08:46 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,14 +9,14 @@
 void print_alphabet(void)
 
 {
-        char x;
+char x;
 
-        for (x = 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 
-        {
+	{
 	_putchar(x);
 
 	}
-	
-       	_putchar('\n');
+
+ _putchar('\n');
 }
