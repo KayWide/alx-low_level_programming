d5ee5162be69098c1f8171a5541cd59faf66350d
KayWide <elkiariey@gmail.com>
2022-11-06 00:13:42 +0300

    elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include <elf.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: argument vector
+ * Return: 0 upon success
+ */
+int main(int argc, char *argv[])
+{
+	return (0)
+}
